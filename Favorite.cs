42c55f01951ae36fcef7cbeb35128a2bc7b032dc
Dũng <114909128+dangtiendung1409@users.noreply.github.com>
2024-03-11 16:34:57 +0700

    Add files via upload

---
+++ Favorite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CrudFavorite
+{
+	public class Favorite
+	{
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+      
+    }
+}
+
